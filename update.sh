4a456e36c71bf51979912562489a7b33e574526d
Jary@cloud <jarysun@outlook.com>
2022-06-02 09:37:22 +0800

    升级脚本（测试）

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git fetch godl &&
+
+git merge godl/master -m "merge goland/dl master" && 
+
+sed -i "s#github.com/SunJary/dl#github.com/SunJary/dl#g"  `grep go -rl .` && 
+
+git commit -a -m"new version" &&
+
+git push origin master
