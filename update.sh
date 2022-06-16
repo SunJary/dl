ade9b3c5092be98e58cfe6ae500f0d49262ee4a9
Jary@cloud <jarysun@outlook.com>
2022-06-16 09:58:16 +0800

    new version

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ git fetch godl &&
 
 git merge godl/master -m "merge goland/dl master" && 
 
-sed -i "s#github.com/SunJary/dl#github.com/SunJary/dl#g"  `grep go -rl .`  && 
+sed -i "s#golang.org/dl#github.com/SunJary/dl#g"  `grep go -rl --include '*.go' .` && 
 
 git commit -a -m"new version" &&
 
